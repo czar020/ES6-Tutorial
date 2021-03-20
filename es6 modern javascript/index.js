956ebd354153600382da57a830b6d3e6bec1b1ff
czarrific <camua.117449@cubao.sti.edu.ph>
2021-03-20 22:42:10 +0800

    Initial Commit

---
+++ es6 modern javascript/index.js
@@ -0,0 +1,114 @@
+// import  Teacher, { promote }  from "./teacher";
+// import React , { Component } from 'react';
+
+
+// const teacher = new Teacher('Czar', "BSIT");
+// teacher.teach();
+//var -> function
+//let -> block 
+//const -> block
+// function sayHello(){
+//     for(let i=0; i<5; i++) {
+//         console.log(i);
+//     }
+
+//     console.log(i);
+// }
+
+// sayHello();
+
+// const x = 1;
+// x = 2;
+
+// const person = {
+//     name: 'Czar',
+//     walk() {},
+//     talk() {}
+// };
+
+// person.talk();
+// person.name= '';
+
+// const targetMember = 'name';
+// person[targetMember.value] = 'Pj';
+
+// const person= {
+//     name: 'Czar',
+//     walk() {
+//         console.log(this);
+//     }
+// };
+
+// person.walk();
+
+// const walk = person.walk.bind(person);
+// walk();
+
+// const square = number => number * number;
+// console.log(square(5));
+
+// const jobs = [
+//     { id: 1, isActive: true},
+//     { id: 2, isActive: true},
+//     { id: 3, isActive: false},
+// ];
+
+// const activeJobs = jobs.filter(job => job.isActive);
+
+// const person = {
+//     talk() {
+//         var self = this;
+//         setTimeout( () => {
+//             console.log("this", this);
+//         }, 1000);
+      
+//     }
+// };
+
+// person.talk();
+
+// const colors = ['red', 'green', 'blue'];
+// const items = colors.map(color => `<li>${color}</li>`);
+// console.log(items);
+
+// const address = {
+//     street: '',
+//     city: '',
+//     country: ''
+// };
+
+// const street = address.street;
+// const city = address.city;
+// const country = address.country;
+
+// const{ street: st } = address;
+
+// const first = [1, 2, 3];
+// const second = [4, 5, 6];
+
+// const combined = first.concat(second);
+// const combined = [...first, 'a', ...second, 'b'];
+
+// const clone = [...first];
+// console.log(first);
+// console.log(clone);
+
+// const first = { name: 'Czar'};
+// const second = { job: 'backend developer'};
+
+// const combined = {...first, ...second, location: 'Australia'};
+// console.log(combined);
+
+// const clone = {...first};
+
+
+
+
+
+
+
+// const person = new Person('Czar');
+// person.
+
+   
+   
